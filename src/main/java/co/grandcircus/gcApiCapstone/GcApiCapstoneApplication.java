a9b53b073fed408404c8b3aa5b21eb8a0548dcc1
jayme <jaymeipema@gmail.com>
2020-11-24 16:17:12 -0500

    initial commit

---
+++ src/main/java/co/grandcircus/gcApiCapstone/GcApiCapstoneApplication.java
@@ -0,0 +1,13 @@
+package co.grandcircus.gcApiCapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcApiCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcApiCapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/grandcircus/gcApiCapstone/GcApiCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package co.grandcircus.gcApiCapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcApiCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
