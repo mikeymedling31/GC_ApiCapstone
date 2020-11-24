d43a569fdd4033e189a123ca82d01c3db41f2e5f
jessperschke <anonymous171@posteo.de>
2020-11-24 16:29:56 -0500

    ingredients and recipeservice classes made

---
+++ src/RecipeService.java
@@ -0,0 +1,5 @@
+package co.grandcircus.gcApiCapstone;
+
+public class RecipeService {
+
+}
---
+++ src/main/java/co/grandcircus/gcApiCapstone/Ingredients.java
@@ -0,0 +1,19 @@
+package co.grandcircus.gcApiCapstone;
+
+public class Ingredients {
+	private String text;
+
+	public Ingredients(String text) {
+		this.text = text;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+}
