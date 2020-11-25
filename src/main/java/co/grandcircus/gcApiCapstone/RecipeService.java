933b32770ed93f9245801a42602b3c436bd6b7d5
jessperschke <anonymous171@posteo.de>
2020-11-25 15:51:31 -0500

    edit RecipeService for new reqs

--- src/main/java/co/grandcircus/gcApiCapstone/RecipeService.java
+++ src/main/java/co/grandcircus/gcApiCapstone/RecipeService.java
@@ -1,14 +1,15 @@
 package co.grandcircus.gcApiCapstone;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class RecipeService {	
-	@Autowired
 	private RestTemplate restTemplate; 
-	
+	public RecipeResponse getById(String r) {
+		RecipeResponse getR = restTemplate.getForObject("https://api.edamam.com/search?r=" + r + "&app_id=96217916&app_key=275ea784f3be2a0c355959c3d3512b41", RecipeResponse.class);		
+		return getR;
+	}
 	public RecipeResponse getAllRecipes(String qId) {
 		RecipeResponse allRecipes = restTemplate.getForObject("https://api.edamam.com/search?q=" + qId + "&app_id=96217916&app_key=275ea784f3be2a0c355959c3d3512b41", RecipeResponse.class);		
 		return allRecipes;
